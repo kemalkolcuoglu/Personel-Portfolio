1bd15eb78163304b909803101c39397b0a40bb7d
Kemal Kolcuoglu <kemal@kolcuoglu.net>
2024-05-09 23:54:15 +0300

    Mail template & form

--- src/resources/views/mail/feedback.blade.php
+++ src/resources/views/mail/feedback.blade.php
@@ -3,7 +3,8 @@
 <h3>Feedback Content:</h3>
 <ul>
   <li><b>Name: </b>{{ $feedbackName }}</li>
-  <li><b>Topic: </b>{{ $feedbackTopic }}</li>
+  <li><b>Topic: </b>{{ $feedbackSubject }}</li>
+  <li><b>Phone: </b>{{ $feedbackPhone }}</li>
   <li><b>E-Mail: </b>{{ $feedbackEmail }}</li>
   <li><b>Message: </b>{{ $feedbackMessage }}</li>
 </ul>
